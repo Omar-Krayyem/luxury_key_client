e7aa01497f6ad057bcf66bfeeddf80d5c256c9c5
Omar-Krayyem <omar.krayyem.b@gmail.com>
2023-11-04 13:47:31 +0200

    Client <> designed header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
+import Header from './components/Header';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
+      <Route path='/header' element={<Header/>} />
     </Routes>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,25 @@
 import './style.css';
+import logo from '../../images/landscape.png'
+import { Avatar } from "@mui/material";
+import profile from '../../images/jordyn.jpeg';
+import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
 
 const Header = () => {
     return (
-        <div>HEader</div>
+        <div className='header'>
+            <div className="left_side">
+                <img src={logo} alt='logo'></img>
+            </div>
+            <div className="right_side">
+                <div className='noti'>
+                    <NotificationsNoneRoundedIcon className='noti_icon' sx={{ fontSize: 30 }}/>
+                </div>
+                <Avatar alt="Profile" src={profile} 
+                sx={{ width: 50, height: 50 }}
+                className='profile_img'
+                />
+            </div>
+        </div>
     );
 }
 
